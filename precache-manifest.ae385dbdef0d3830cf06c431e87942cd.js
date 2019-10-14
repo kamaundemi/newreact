d78f491a2cc8dc1543a634f962b12c58d6c34e74
kamaundemi <georgendemi@yahoo.com>
2019-10-13 23:06:41 -0500

    Updates

---
+++ precache-manifest.ae385dbdef0d3830cf06c431e87942cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9784182f7482c06050c543773100164b",
+    "url": "/newreact/index.html"
+  },
+  {
+    "revision": "719c8f74448c6f05b700",
+    "url": "/newreact/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7204c04aef3e99816bb6",
+    "url": "/newreact/static/css/main.4a0a1701.chunk.css"
+  },
+  {
+    "revision": "719c8f74448c6f05b700",
+    "url": "/newreact/static/js/2.ec1cd35a.chunk.js"
+  },
+  {
+    "revision": "7204c04aef3e99816bb6",
+    "url": "/newreact/static/js/main.a95b9efa.chunk.js"
+  },
+  {
+    "revision": "9e4682b43d32e4411d69",
+    "url": "/newreact/static/js/runtime-main.369048f8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/newreact/static/media/SEGA.12a3db8b.woff"
+  }
+]);
